960d8f20cc60ed91c9efba36885d9a4f243bfdb3
Cicada <89372746+stephen198400@users.noreply.github.com>
2025-02-21 07:23:31 +0800

    feat: add Google Tag Manager integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,15 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
+				<Script id="google-tag-manager" strategy="afterInteractive">
+					{`
+						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-M4PN2ZCT');
+					`}
+				</Script>
 				<Script
 					src="https://www.googletagmanager.com/gtag/js?id=AW-16858007389"
 					strategy="afterInteractive"
@@ -61,6 +70,14 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-M4PN2ZCT"
+						height="0"
+						width="0"
+						style={{ display: 'none', visibility: 'hidden' }}
+					></iframe>
+				</noscript>
 				{children}
 			</body>
 		</html>
